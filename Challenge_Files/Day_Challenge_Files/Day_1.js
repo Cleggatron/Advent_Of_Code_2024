ed87ad68ee15770408e1a9c7f84dd7abe9c910d5
Cleggatron <david.robert.clegg@gmail.com>
2024-12-02 21:54:01 +0000

    added challenge specific file formatting

--- Challenge_Files/Day_Challenge_Files/Day_1.js
+++ Challenge_Files/Day_Challenge_Files/Day_1.js
@@ -5,3 +5,20 @@ const file_read = () => {
     return file_contents;
 }
 
+
+//Takes the string and splits by line break, then by spaces, and returns the two arrays
+const format_file_contents = (file_text) => {
+    let arr1 = [];
+    let arr2 = [];
+    let lines = file_text.replaceAll("\r","").split("\n");
+    for(let line of lines){
+        let [num1, num2] = line.split("   ");
+        arr1.push(num1);
+        arr2.push(num2);
+    }
+    return [arr1, arr2]
+}
+
+
+
+
