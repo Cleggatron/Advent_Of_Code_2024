a6dcc20ad82ddfbb43d8a742841960c6fc91e85e
Cleggatron <david.robert.clegg@gmail.com>
2024-12-16 10:15:50 +0000

    day two complete

--- Challenge_Files/Day_Challenge_Files/Day_3.js
+++ Challenge_Files/Day_Challenge_Files/Day_3.js
@@ -12,4 +12,40 @@ const multiply_mul = (mul) => {
     return num_1 * num_2; 
 }
 
+let total = 0;
+
+for(let mul of muls){
+    total+= multiply_mul(mul);
+}
+
+const enhanced_mul_regex = /mul\(\d+,\d+\)|(do\(\))|don't\(\)/g
+const enhanced_muls = file_contents.match(enhanced_mul_regex); 
+
+const handle_enhanced_muls = (muls) => {
+    let enabled = true;
+    let total = 0;
+    const do_regex = /do\(\)/
+    const do_dont_regex = /do\(\)|don't\(\)/g;
+
+    for(let mul of muls){
+        if(mul.match(do_dont_regex)){
+            if(mul.match(do_regex)){
+                enabled = true;
+            } else {
+                enabled = false
+            }
+            continue;
+        }
+
+        if(enabled){
+            total += multiply_mul(mul);
+        }
+    }
+
+    return total;
+}
+
+console.log(handle_enhanced_muls(enhanced_muls));
+
+
 
