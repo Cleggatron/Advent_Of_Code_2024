707690dcbee1d62a7ed037821dece0c82d065d0a
Cleggatron <david.robert.clegg@gmail.com>
2024-12-16 09:18:22 +0000

    day 2 f. Not functioning

--- Challenge_Files/Day_Challenge_Files/Day_2.js
+++ Challenge_Files/Day_Challenge_Files/Day_2.js
@@ -1,4 +1,4 @@
-import file_read from "../HelperFunctions/helpers.js"
+import {file_read, array_reverse } from "../HelperFunctions/helpers.js"
 
 const file_contents = file_read("Input_02.txt");
 
@@ -7,6 +7,45 @@ const lines_to_array = (data) => {
     return lines
 }
 
-console.log(file_contents);
+const check_increasing = (arr) => {
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] < arr[i-1] || (arr[i] - arr[i-1]) < 1 || (arr[i] - arr[i-1]) > 3){
+            console.log(arr)
+        
+            return false;
+        }
+    }
+    return true;
+}
 
-console.log(lines_to_array(file_contents))
+const check_decreasing = (arr) => {
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i-1] < arr[i] || (arr[i-1] - arr[i]) < 1 || (arr[i-1] - arr[i]) > 3){
+            console.log(arr)
+            return false;
+        }
+    }
+    return true;
+}
+
+const check_validity = (line) => {
+    const increasing = line[0] < line[line.length - 1];
+    
+    if(increasing){
+        return check_increasing(line);
+    } else {
+        return check_decreasing(line);
+    }
+}
+
+const count_valid = (arr) => {
+    let total = 0;
+    for(let line of arr) {
+        if(check_validity(line)){
+            total++;
+        }
+    }
+    return total;
+}
+console.log(count_valid(lines_to_array(file_contents)));
+
