1dae6d1fe15e4fd9248e1e188a68fe9f821024ba
Cleggatron <david.robert.clegg@gmail.com>
2024-12-16 10:15:39 +0000

    part one complete

--- Challenge_Files/Day_Challenge_Files/Day_3.js
+++ Challenge_Files/Day_Challenge_Files/Day_3.js
@@ -1,13 +1,15 @@
 import {file_read} from "../HelperFunctions/helpers.js";
 
 
-const file_contents = file_read("Input_03.txt")
+const file_contents = file_read("Input_03.txt");
 
-const mul_regex = /mul\(\d+,\d+\)/g
+const mul_regex = /mul\(\d+,\d+\)/g;
 
-const muls = file_contents.match(mul_regex)
+const muls = file_contents.match(mul_regex);
 
+const multiply_mul = (mul) => {
+    const [num_1, num_2] = mul.replace(/([mul\(\)])/g, "").split(",");
+    return num_1 * num_2; 
+}
 
-
-console.log(muls)
 
